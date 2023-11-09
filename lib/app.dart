34b5c2541125c8b98b5e0df9c1092c40f70cf541
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2023-11-09 10:01:10 +0100

    Inline status bar theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template/modules/counter/counter.dart';
 import 'package:flutter_template/theme/color_schemes.dart';
-import 'package:flutter_template/theme/status_bar.dart';
 
 class AwesomeApp extends ConsumerWidget {
   const AwesomeApp({super.key});
@@ -12,9 +12,27 @@ class AwesomeApp extends ConsumerWidget {
     useTransparentStatusBar();
     return MaterialApp(
       title: 'Awesome New App Name',
-      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
-      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: lightColorScheme,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
+      ),
       home: const RiverpodCounterPage(),
+    );
+  }
+
+  void useTransparentStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+      ),
     );
   }
 }
