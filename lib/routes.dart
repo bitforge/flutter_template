de167c3c1ca62d7707276cbc3b9bbd86936ac649
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2023-11-13 18:00:11 +0100

    Remove navigator keys

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,42 +1,26 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_template/modules/cats/routes.dart';
 import 'package:flutter_template/modules/home/routes.dart';
 import 'package:flutter_template/modules/web/routes.dart';
 import 'package:flutter_template/shell.dart';
 import 'package:go_router/go_router.dart';
 
-final rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root_navigator');
-final homeNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'home_navigator');
-final catsNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'cats_navigator');
-final webNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'web_navigator');
-final moreNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'more_navigator');
-
 final router = GoRouter(
-  navigatorKey: rootNavigatorKey,
   initialLocation: '/home',
-  debugLogDiagnostics: true,
+  debugLogDiagnostics: kDebugMode,
   routes: <RouteBase>[
     /// Holds all tab root pages
     StatefulShellRoute.indexedStack(
       builder: (context, state, navigationShell) => RootNavigationShell(navigationShell: navigationShell),
       branches: [
-        /// Home tab
-        StatefulShellBranch(
-          navigatorKey: homeNavigatorKey,
-          routes: homeRoutes,
-        ),
+        /// Home
+        StatefulShellBranch(routes: homeRoutes),
 
-        /// Cats tab
-        StatefulShellBranch(
-          navigatorKey: catsNavigatorKey,
-          routes: catRoutes,
-        ),
+        /// Cats
+        StatefulShellBranch(routes: catRoutes),
 
-        /// Web tab
-        StatefulShellBranch(
-          navigatorKey: webNavigatorKey,
-          routes: webRoutes,
-        ),
+        /// Web
+        StatefulShellBranch(routes: webRoutes),
       ],
     ),
   ],
