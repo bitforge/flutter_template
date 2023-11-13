dfea167f143772bcc6a3f1def0dffa7b06d88214
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2023-11-13 18:19:21 +0100

    Add simple webview page

--- lib/modules/web/pages/web_page.dart
+++ lib/modules/web/pages/web_page.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/theme/custom_colors.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class WebPage extends StatelessWidget {
+class WebPage extends StatefulWidget {
   const WebPage({super.key});
+
+  @override
+  State<WebPage> createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> {
+  /// WebViewController
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(CustomColors.bitforgeBlack)
+      ..loadRequest(Uri.parse('https://bitforge.ch'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Bitforge Homepage')),
-      body: const Center(
-        child: Text('A WebView page'),
-      ),
+      body: WebViewWidget(controller: controller),
     );
   }
 }
---
+++ lib/theme/custom_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color bitforgeBlack = Color(0xFF0A0908);
+}
