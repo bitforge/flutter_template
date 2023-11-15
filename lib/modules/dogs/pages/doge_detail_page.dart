912675397640f20333b53f182e1fd02959c75655
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2023-11-15 16:38:26 +0100

    Share doges, spread the ❤️

--- lib/modules/dogs/pages/doge_detail_page.dart
+++ lib/modules/dogs/pages/doge_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/modules/dogs/widgets/shibe_image.dart';
+import 'package:share_plus/share_plus.dart';
 
 class DogeDetailPage extends StatelessWidget {
   const DogeDetailPage({required this.imageUrl, super.key});
@@ -11,10 +12,20 @@ class DogeDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Such Doge! Much wow!'),
+        actions: [
+          IconButton(
+            onPressed: () => _shareUrl(imageUrl),
+            icon: const Icon(Icons.share),
+          )
+        ],
       ),
       body: Center(
         child: ShibeImage(imageUrl: imageUrl),
       ),
     );
+  }
+
+  void _shareUrl(String imageUrl) {
+    Share.share(imageUrl, subject: 'Such Doge! Much wow!');
   }
 }
