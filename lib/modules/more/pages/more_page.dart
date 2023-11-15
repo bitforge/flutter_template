cb8a861f5c8a05b012508892fef00aa8f39a1e57
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2023-11-16 00:21:06 +0100

    Add language change

--- lib/modules/more/pages/more_page.dart
+++ lib/modules/more/pages/more_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:app_settings/app_settings.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -20,6 +23,13 @@ class MorePage extends StatelessWidget {
                 onPressed: () => _launchBrowser('https://bitforge.ch'),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: ElevatedButton(
+                child: Text('more.change_language'.tr()),
+                onPressed: () => _changeLanguage(context),
+              ),
+            ),
           ],
         ),
       ),
@@ -30,5 +40,15 @@ class MorePage extends StatelessWidget {
     final uri = Uri.parse(url);
     // 💡: Try other launch modes
     launchUrl(uri, mode: LaunchMode.inAppWebView);
+  }
+
+  void _changeLanguage(BuildContext context) {
+    if (Platform.isAndroid || Platform.isIOS) {
+      AppSettings.openAppSettings(type: AppSettingsType.appLocale);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('errors.not_implemented_yet'.tr())),
+      );
+    }
   }
 }
