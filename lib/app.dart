36b4b47ed21de8518cb5513fd82efcc542605527
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2023-11-20 10:28:38 +0100

    Use Material 3 theme by default

--- lib/app.dart
+++ lib/app.dart
@@ -2,57 +2,31 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_template/router.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_template/theme/color_schemes.dart';
+import 'package:flutter_template/theme/text_theme.dart';
 
 class AwesomeApp extends StatelessWidget {
   const AwesomeApp({super.key});
-
-  // Your app name
-  static const title = 'Awesome New App Name';
-
-  // Basic initial Material 2 color theme
-  //
-  // Start with a Material theme builder:
-  // - Offical Theme Builder (Google): https://m3.material.io/theme-builder
-  // - Appainter (Has color palettes): https://appainter.dev
-  //
-  // Also check the official Figma Material 3 Desgin Kit:
-  // https://www.figma.com/community/file/1035203688168086460
-  //
-  // This template uses Material 2 by default, since it`s simple & colorful to start with.
-  // When you're ready, it's recommended to switch to a dynamic or custom Material 3 theme.
-  static const useMaterial3 = false;
-  static const seedColor = Colors.pink;
 
   @override
   Widget build(BuildContext context) {
     useTransparentStatusBar();
     return MaterialApp.router(
-      title: title,
+      // Your app name
+      title: 'Awesome New App Name',
       routerConfig: router,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
       theme: ThemeData(
-        useMaterial3: useMaterial3,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: seedColor,
-          secondary: Colors.amberAccent.shade700,
-        ),
-        textTheme: GoogleFonts.robotoTextTheme(
-          ThemeData.light().textTheme,
-        ),
+        useMaterial3: true,
+        colorScheme: lightColorScheme,
+        textTheme: customTextTheme,
       ),
       darkTheme: ThemeData(
-        useMaterial3: useMaterial3,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: seedColor,
-          secondary: Colors.grey.shade100,
-          brightness: Brightness.dark,
-        ),
-        textTheme: GoogleFonts.robotoTextTheme(
-          ThemeData.dark().textTheme,
-        ),
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
+        textTheme: customTextTheme,
       ),
     );
   }
---
+++ lib/themes/text_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final TextTheme customTextTheme = TextTheme(
+  displayLarge: GoogleFonts.roboto(fontSize: 57, fontWeight: FontWeight.w400, letterSpacing: -0.25),
+  displayMedium: GoogleFonts.roboto(fontSize: 45, fontWeight: FontWeight.w400),
+  displaySmall: GoogleFonts.roboto(fontSize: 36, fontWeight: FontWeight.w400),
+  headlineLarge: GoogleFonts.roboto(fontSize: 32, fontWeight: FontWeight.w400),
+  headlineMedium: GoogleFonts.roboto(fontSize: 28, fontWeight: FontWeight.w400),
+  headlineSmall: GoogleFonts.roboto(fontSize: 24, fontWeight: FontWeight.w400),
+  titleLarge: GoogleFonts.roboto(fontSize: 22, fontWeight: FontWeight.w400),
+  titleMedium: GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+  titleSmall: GoogleFonts.roboto(fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  bodyLarge: GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+  bodyMedium: GoogleFonts.roboto(fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  bodySmall: GoogleFonts.roboto(fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+  labelLarge: GoogleFonts.roboto(fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  labelMedium: GoogleFonts.roboto(fontSize: 12, fontWeight: FontWeight.w500, letterSpacing: 0.5),
+  labelSmall: GoogleFonts.roboto(fontSize: 11, fontWeight: FontWeight.w500, letterSpacing: 0.5),
+);
