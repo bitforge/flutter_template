c16ff45c632f373a9a74376b2d50d68687d4fb17
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2023-11-21 21:47:54 +0100

    Extract app info sheet

--- lib/modules/global/actions/show_app_info.dart
+++ lib/modules/global/actions/show_app_info.dart
@@ -1,44 +1,15 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/constants/keys.dart';
-import 'package:flutter_template/modules/global/info.dart';
-import 'package:gap/gap.dart';
+import 'package:flutter_template/modules/global/widgets/app_info_sheet.dart';
 
 void showAppInfo(BuildContext context) {
-  final theme = Theme.of(context);
   showModalBottomSheet<void>(
+    // Global overlay
     context: rootNavigatorKey.currentState!.context,
+    // Local overlay
+    //context: context,
     builder: (BuildContext context) {
-      return SizedBox(
-        height: 200,
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(
-                  'info.text1'.tr(),
-                  style: theme.textTheme.titleLarge,
-                  textAlign: TextAlign.center,
-                ),
-                const Gap(16),
-                Text(
-                  'info.text2'.tr(namedArgs: {'version': packageInfo.version}),
-                  style: theme.textTheme.bodyMedium,
-                  textAlign: TextAlign.center,
-                ),
-                const Gap(16),
-                ElevatedButton(
-                  child: Text('info.close'.tr()),
-                  onPressed: () => Navigator.pop(context),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
+      return const AppInfoSheet();
     },
   );
 }
---
+++ lib/modules/global/widgets/app_info_sheet.dart
@@ -0,0 +1,41 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_template/modules/global/info.dart';
+import 'package:gap/gap.dart';
+
+class AppInfoSheet extends StatelessWidget {
+  const AppInfoSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'info.text1'.tr(),
+              style: theme.textTheme.titleLarge,
+              textAlign: TextAlign.center,
+            ),
+            const Gap(16),
+            Text(
+              'info.text2'.tr(namedArgs: {'version': packageInfo.version}),
+              style: theme.textTheme.bodyMedium,
+              textAlign: TextAlign.center,
+            ),
+            const Gap(16),
+            ElevatedButton(
+              child: Text('info.close'.tr()),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
