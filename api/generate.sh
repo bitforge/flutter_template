12cf2c849796e7eaa77337ee77860d9f7fa595b1
Elin Angel de Wit <97593927+Elinsama@users.noreply.github.com>
2025-02-05 11:01:40 +0100

    added genarator

---
+++ api/generate.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+cd "$(dirname $0)"
+
+# Use `./generate.sh --localhost` to build against locally running badibuddy-api
+url="https://catfact.ninja/docs/api-docs.json"
+
+# remove generated folders
+rm -f -r ./doc
+rm -f -r ./lib
+rm -f -r ./test
+rm -f -r ./openapi-generator
+
+# Pin openapi-generator version
+# version info available at https://github.com/OpenAPITools/openapi-generator/releases
+npx @openapitools/openapi-generator-cli version-manager set 7.10.0
+
+# generate models and api (no docs nor tests)
+# global properties: https://openapi-generator.tech/docs/globals
+# dart-dio properties: https://openapi-generator.tech/docs/generators/dart-dio
+# built_value -> default or json_serializable
+npx @openapitools/openapi-generator-cli generate \
+    -i "$url" \
+    -g dart-dio \
+    -o ./ \
+    --additional-properties=serializationLibrary=json_serializable \
+    --additional-properties=pubName=catfacts \
+    --additional-properties=pubVersion=0.0.1 \
+    --additional-properties=pubAuthor=miguelseglias \
+    --skip-validate-spec
+    
+# Remove empty tests
+rm -rf test
+
+# Upgrade dependencies
+dart pub upgrade --major-versions
+
+# get packages
+dart pub get
+
+# build
+dart run build_runner build --delete-conflicting-outputs
+
+# Auto format all dart files
+dart format . -l 120
+
+CLR='\033[0;32m'
+EOC='\033[0m'
+printf "\n${CLR}CLIENT GENERATED SUCCESSFULLY${EOC}\n"
